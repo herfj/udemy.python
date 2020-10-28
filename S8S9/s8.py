048eb954fd8d4f4627f99714872f6732a871759a
Herfj <54822117+Herfj@users.noreply.github.com>
2020-10-28 13:08:34 -0300

    va

---
+++ S8S9/s8.py
@@ -0,0 +1,30 @@
+# i = 0
+# while i < 5:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# lista = ['A','B','C','D','F','E']
+
+# for letra in lista:
+#     print(letra)
+
+# usuario = "hernann"
+
+# for ch in usuario:
+#     if (ch == "n"):
+#         continue
+#     print(ch)
+# 
+# for x in range(3,30,3):
+#     print(x);
+# else:
+#     print("temine de hacer el for!")
+
+# usuarios = ['hernan', 'tomi', 'jacomo,', 'laucha']
+# edades = [324,345,32,43]
+
+# for usuario in usuarios:
+#     for edad in edades:
+#         print(usuario, edad)
